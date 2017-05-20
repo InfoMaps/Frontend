1c3b0917e6e4e810357fb31bbaaf657c16b48e93
tudi2d <phugenroth@googlemail.com>
2017-05-20 21:13:09 +1000

    Implementing LGA Data

--- src/components/content/Map.jsx
+++ src/components/content/Map.jsx
@@ -15,8 +15,23 @@ export default class Map extends React.Component{
     initMap() {
         let brisbane = {lat: -27.3818611, lng: 152.7130136};
         let map = new google.maps.Map(this.map, {
-            zoom: 5,
+            zoom: 7,
             center: brisbane
+        });
+        map.data.loadGeoJson(
+            'https://raw.githubusercontent.com/thomjoy/aus-lga/master/data/aus_lga.json');
+        console.log(map.data.b.b);
+        let i = 0;
+        map.data.addListener('addfeature', function(event) {
+            i++;
+            if(i===565){
+                for(let i in map.data.b.b){
+                    console.log(map.data.b.b[i].f.LGA_CODE11)
+                }
+            }
+        });
+        map.data.addListener('click', function(event) {
+            map.data.overrideStyle(event.feature, {fillColor: 'red'});
         });
     }
 
